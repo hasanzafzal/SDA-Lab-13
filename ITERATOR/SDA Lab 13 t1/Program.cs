d97ad84c5c1b9c6a74776204eb96b0c56f49d733
Hasan Zahid <hasanzafzal@gmail.com>
2025-05-22 10:49:40 +0500

    Initial commit

---
+++ ITERATOR/SDA Lab 13 t1/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace NotificationIteratorPattern
+{
+    // A simple Notification class
+    class Notification
+    {
+        private string notification;
+
+        public Notification(string notification)
+        {
+            this.notification = notification;
+        }
+
+        public string GetNotification()
+        {
+            return notification;
+        }
+    }
+
+    // Iterator interface
+    interface IIterator
+    {
+        bool HasNext();
+        object Next();
+    }
+
+    // Collection interface
+    interface ICollection
+    {
+        IIterator CreateIterator();
+    }
+
+    // Collection of notifications
+    class NotificationCollection : ICollection
+    {
+        private static readonly int MAX_ITEMS = 6;
+        private int numberOfItems = 0;
+        private Notification[] notificationList;
+
+        public NotificationCollection()
+        {
+            notificationList = new Notification[MAX_ITEMS];
+
+            // Add some dummy notifications
+            AddItem("Notification 1");
+            AddItem("Notification 2");
+            AddItem("Notification 3");
+        }
+
+        public void AddItem(string str)
+        {
+            Notification notification = new Notification(str);
+            if (numberOfItems >= MAX_ITEMS)
+                Console.Error.WriteLine("Full");
+            else
+            {
+                notificationList[numberOfItems] = notification;
+                numberOfItems++;
+            }
+        }
+
+        public IIterator CreateIterator()
+        {
+            return new NotificationIterator(notificationList);
+        }
+    }
+
+    // Notification iterator
+    class NotificationIterator : IIterator
+    {
+        private Notification[] notificationList;
+        private int pos = 0;
+
+        public NotificationIterator(Notification[] notificationList)
+        {
+            this.notificationList = notificationList;
+        }
+
+        public bool HasNext()
+        {
+            return pos < notificationList.Length && notificationList[pos] != null;
+        }
+
+        public object Next()
+        {
+            Notification notification = notificationList[pos];
+            pos++;
+            return notification;
+        }
+    }
+
+    // Contains collection of notifications
+    class NotificationBar
+    {
+        private NotificationCollection notifications;
+
+        public NotificationBar(NotificationCollection notifications)
+        {
+            this.notifications = notifications;
+        }
+
+        public void PrintNotifications()
+        {
+            IIterator iterator = notifications.CreateIterator();
+            Console.WriteLine("-------NOTIFICATION BAR------------");
+            while (iterator.HasNext())
+            {
+                Notification n = (Notification)iterator.Next();
+                Console.WriteLine(n.GetNotification());
+            }
+        }
+    }
+
+    // Main driver
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotificationCollection nc = new NotificationCollection();
+            NotificationBar nb = new NotificationBar(nc);
+            nb.PrintNotifications();
+
+            Console.ReadLine(); // To keep console open
+        }
+    }
+}
---
+++ MEDIATOR/SDA Lab 13 t2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace MediatorPattern
+{
+    // Mediator Interface
+    interface IATCMediator
+    {
+        void RegisterRunway(Runway runway);
+        void RegisterFlight(Flight flight);
+        bool IsLandingOk();
+        void SetLandingStatus(bool status);
+    }
+
+    // Concrete Mediator
+    class ATCMediator : IATCMediator
+    {
+        private Flight flight;
+        private Runway runway;
+        private bool land;
+
+        public void RegisterRunway(Runway runway)
+        {
+            this.runway = runway;
+        }
+
+        public void RegisterFlight(Flight flight)
+        {
+            this.flight = flight;
+        }
+
+        public bool IsLandingOk()
+        {
+            return land;
+        }
+
+        public void SetLandingStatus(bool status)
+        {
+            land = status;
+        }
+    }
+
+    // Command Interface
+    interface ICommand
+    {
+        void Land();
+    }
+
+    // Colleague - Flight
+    class Flight : ICommand
+    {
+        private IATCMediator atcMediator;
+
+        public Flight(IATCMediator atcMediator)
+        {
+            this.atcMediator = atcMediator;
+        }
+
+        public void Land()
+        {
+            if (atcMediator.IsLandingOk())
+            {
+                Console.WriteLine("Successfully Landed.");
+                atcMediator.SetLandingStatus(true);
+            }
+            else
+            {
+                Console.WriteLine("Waiting for landing.");
+            }
+        }
+
+        public void GetReady()
+        {
+            Console.WriteLine("Ready for landing.");
+        }
+    }
+
+    // Colleague - Runway
+    class Runway : ICommand
+    {
+        private IATCMediator atcMediator;
+
+        public Runway(IATCMediator atcMediator)
+        {
+            this.atcMediator = atcMediator;
+            atcMediator.SetLandingStatus(true); // Grant permission immediately
+        }
+
+        public void Land()
+        {
+            Console.WriteLine("Landing permission granted.");
+            atcMediator.SetLandingStatus(true);
+        }
+    }
+
+    // Main Program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IATCMediator atcMediator = new ATCMediator();
+
+            Flight sparrow101 = new Flight(atcMediator);
+            Runway mainRunway = new Runway(atcMediator);
+
+            atcMediator.RegisterFlight(sparrow101);
+            atcMediator.RegisterRunway(mainRunway);
+
+            sparrow101.GetReady();
+            mainRunway.Land();
+            sparrow101.Land();
+
+            Console.ReadLine(); // Keep console window open
+        }
+    }
+}
